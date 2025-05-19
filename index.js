336070cf1e83a078f053a75b5e5b271caa4709c2
bernat alcudia <bernatalcudia@gmail.com>
2025-05-19 09:14:31 +0200

    feat(api):create routes in api

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const port = 8000
 const cors = require('cors')
 const express = require('express')
 const { dbConnection } = require('./db')
+const { auth } = require('./middlewares/auth')
+
+const recipesRoutes = require('./routes/recipes.routes')
+const ingredientsRoutes = require('./routes/ingredients.routes')
+const usersRoutes = require('./routes/users.routes')
 
 
 const main = () => {
     const app = express()
     app.use(cors())
     app.use(express.json())
+
+    app.use('/recipes', recipesRoutes)
+    app.use('/ingredients', ingredientsRoutes)
+    app.use('/', usersRoutes)
 
     dbConnection()
 
