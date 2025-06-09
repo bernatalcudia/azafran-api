db240759b2871f0823c77835da5f1b43d698f97a
bernat alcudia <bernatalcudia@gmail.com>
2025-06-09 10:46:05 +0200

    feat(api):implement login in user.controller.js and fix  register

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,8 +1,30 @@
 const { User } = require('../models/user.model')
 const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../config.js')
 
 const login = async (req, res) => {
     const { username, password } = req.body
+    if (!username || !password) {
+        res.status(404).send('Missing username or password')
+        return
+    }
+
+    const user = User.findOne({ username: username })
+
+    if (!user) {
+        res.status(404).send({ msg: 'invalid credentials' })
+        return
+    }
+
+    const isPasswordMatch = bcryptjs.compareSync(password, user.password)
+    if (!isPasswordMatch) {
+        res.status(404).send({ msg: 'invalid credentials' })
+        return
+    }
+
+    const accessToken = jwt.sign({ userId: user._id }, JWT_SECRET)
+    res.send({ accessToken })
 }
 
 
@@ -16,7 +38,8 @@ const register = async (req, res) => {
     const hashedPassword = bcryptjs.hashSync(password)
 
     try {
-
+        const user = new User({ username: username, password: hashedPassword })
+        await user.save()
     } catch (error) {
         console.log(error)
         if (error.errorResponse.errmsg) {
